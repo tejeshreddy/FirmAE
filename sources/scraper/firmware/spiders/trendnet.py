2667fdf02153303e5490e1a399d021dc17416700
tejeshreddy <tejeshreddy111@gmail.com>
2022-02-28 12:49:09 -0700

    feat: trendnet

--- sources/scraper/firmware/spiders/trendnet.py
+++ sources/scraper/firmware/spiders/trendnet.py
@@ -1,57 +1,43 @@
+from asyncio.log import logger
+from calendar import firstweekday
+from distutils.debug import DEBUG
+from fileinput import filename
+from gc import callbacks
+from http.client import ResponseNotReady
+from urllib import request
 from scrapy import Spider
 from scrapy.http import Request
-
+import time
 from firmware.items import FirmwareImage
 from firmware.loader import FirmwareLoader
-
+import os
+import json
 import urllib.parse
+import wget
+import logging
+from scrapy.utils.log import configure_logging 
 
 class TrendnetSpider(Spider):
+    handle_httpstatus_list = [404]
     name = "trendnet"
     allowed_domains = ["trendnet.com"]
-    start_urls = ["http://www.trendnet.com/support/"]
+    start_urls = ["http://downloads.trendnet.com/"]
 
     def parse(self, response):
-        for entry in response.xpath("//select[@id='SUBTYPE_ID']/option"):
-            if entry.xpath(".//text()"):
-                text = entry.xpath(".//text()").extract()[0]
-                href = entry.xpath("./@value").extract()[0]
+        base_url = "http://downloads.trendnet.com/"
 
-                yield Request(
-                    url=urllib.parse.urljoin(response.url, href),
-                    meta={"product": text},
-                    headers={"Referer": response.url},
-                    callback=self.parse_product)
+        for entry in response.xpath("/html/body/pre/a/@href").extract():
+            yield Request(
+                url=urllib.parse.urljoin(base_url, entry + "firmware"),
+                callback=self.parse_products
+            )
+    
+    def parse_products(self, response):
+        with open("trendnet-metadata.txt", "a") as fp:
+            for entry in response.xpath("/html/body/pre/a/@href").extract():
+                fp.write(str(urllib.parse.urljoin(response.url, entry)))
+                fp.write("\n")
 
-    def parse_product(self, response):
-        for tab in response.xpath("//ul[@class='etabs']//a"):
-            text = tab.xpath(".//text()").extract()[0]
-            href = tab.xpath("./@href").extract()[0]
 
-            if "downloads" in text.lower():
-                yield Request(
-                    url=urllib.parse.urljoin(response.url, href),
-                    meta={"product": response.meta["product"]},
-                    headers={"Referer": response.url},
-                    callback=self.parse_download)
+        
 
-    def parse_download(self, response):
-        for entry in response.xpath("//div[@class='downloadtable']"):
-            text = entry.xpath(".//text()").extract()
-
-            if "firmware" in " ".join(text).lower():
-                text = entry.xpath(
-                    ".//li[@class='maindescription' and position() = 1]//text()").extract()
-                date = entry.xpath(
-                    ".//li[@class='maindescription' and position() = 2]//text()").extract()
-                href = entry.xpath(".//li[@class='maindescription']//a/@onclick").extract()[
-                    0].split('\'')[1] + "&button=Continue+with+Download&Continue=yes"
-
-                item = FirmwareLoader(
-                    item=FirmwareImage(), response=response, date_fmt=["%m/%d/%Y"])
-                item.add_value("url", href)
-                item.add_value("product", response.meta["product"])
-                item.add_value("date", item.find_date(date))
-                item.add_value("version", FirmwareLoader.find_version(text))
-                item.add_value("vendor", self.name)
-                yield item.load_item()
